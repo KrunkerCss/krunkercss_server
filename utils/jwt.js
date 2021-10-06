8064754ed3c0531c29ec1a7d8fef184fd0685073
Sourav Gain <souravgain605@gmail.com>
2021-10-06 19:55:39 +0530

    using env var for token validity

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,11 +1,13 @@
 const Jwt = require("jsonwebtoken");
 
 const create_access_token = (payload) => {
-  return Jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: "24h" });
+  return Jwt.sign(payload, process.env.TOKEN_SECRET, {
+    expiresIn: process.env.TOKEN_VALIDITY,
+  });
 };
 
 const verfiy_access_token = (token) => {
-  return Jwt.verify(token, process.env.TOKEN_SECRET);
+  return Jwt.verify(token, process.env.TOKEN_SECRET || "24h");
 };
 
 module.exports = { create_access_token, verfiy_access_token };
