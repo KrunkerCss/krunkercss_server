0ce213a1caa4cad5fc202f6bd0e78ea2a3c266a0
Sourav Gain <souravgain605@gmail.com>
2021-12-09 20:10:36 +0530

    added self route

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const { HandleError } = require("../utils/error");
 
 router.use("/", require("./auth"));
 router.use("/user", require("./user"));
+router.use("/self", require("./self"));
 router.use("/css", require("./css"));
 router.use("/commission", require("./commission"));
 router.use("/request", require("./request"));
---
+++ routes/self.js
@@ -0,0 +1,23 @@
+const { jwt_check } = require("../middlewares/auth");
+const { allowSecondLevel } = require("../middlewares/role");
+const { HandleError } = require("../utils/error");
+
+const CssModel = require("../db/models/css");
+const CommissionModel = require("../db/models/commission");
+
+const router = require("express").Router();
+
+router.get("/", jwt_check, allowSecondLevel, async (req, res) => {
+  try {
+    const csses = await CssModel.find({ user: req.user.id });
+    const commisions = await CommissionModel.find({ user: req.user.id });
+
+    return res
+      .status(200)
+      .json({ success: true, payload: { csses, commisions } });
+  } catch (err) {
+    return HandleError(err, res);
+  }
+});
+
+module.exports = router;
